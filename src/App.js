26143d19ae3ced278436bf32e653f2cbab358b2b
Corneliu Laur <cl179971@gmail.com>
2022-05-05 10:37:12 +0100

    made the app responsive

--- src/App.js
+++ src/App.js
@@ -2,25 +2,46 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 const App = () => {
-    // const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${apiKey}`;
-    // const apiKey = '14344ba9e89ef7b6496eb2d76a0c293b';
+    const [data, setData] = useState({});
+    const [location, setLocation] = useState('');
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=14344ba9e89ef7b6496eb2d76a0c293b`;
+
+    const getLocation = async () => {
+        const response = await axios.get(url);
+        setData(response.data);
+        console.log(response.data);
+
+        setLocation('');
+    };
 
     return (
         <div className='app'>
             <div className='container'>
                 <div className='top'>
-                    <div className='location'>
-                        <p>London</p>
-                    </div>
+                    <form>
+                        <label>
+                            <input type='text' placeholder='Enter City...' />
+                        </label>
+                        <input
+                            className='button'
+                            type='submit'
+                            onClick={getLocation}
+                            value='Get Weather'
+                        />
+                    </form>
                 </div>
                 <div className='middle'>
+                    <div className='location'>
+                        <p>{data.name}</p>
+                    </div>
                     <div className='temperature'>
-                        <h1>15 C</h1>
+                        {data.main ? <h1>{data.main.temp.toFixed()}</h1> : null}
                     </div>
                 </div>
                 <div className='bottom'>
                     <div className='description'>
-                        <p>Cloudy</p>
+                        {data.weather ? <p>{data.weather[0].main}</p> : null}
                     </div>
                 </div>
             </div>
